d4657accaff68fe9dd8f368f598ec19d4bf273e7
JimSadler <jim@jsdevelopment.tech>
2020-07-07 10:44:02 -0400

    adjusted links in modals

--- components/Modals/Modal4a.vue
+++ components/Modals/Modal4a.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="modal_exp">
-    <b-modal id="modal-scrollable4" scrollable size="xl" title="DataVault Website">
+    <b-modal
+      id="modal-scrollable4"
+      scrollable
+      size="xl"
+      title="DataVault Website"
+    >
       <b-container fluid>
         <b-row>
           <b-col cols="12">
@@ -17,65 +22,64 @@
               @sliding-start="onSlideStart"
               @sliding-end="onSlideEnd"
             >
-                <b-carousel-slide 
-                    img-src="~assets/img/portfolio/DataVault/DV_6.png" 
-                    img-alt 
-                    width="620" 
-                    height="400"
-                ></b-carousel-slide>
-                    <b-carousel-slide
-                    img-src="~assets/img/portfolio/DataVault/DV_5.png"
-                    img-alt
-                    width="620"
-                    height="400"
-                ></b-carousel-slide>
-                <b-carousel-slide
-                    img-src="~assets/img/portfolio/DataVault/DV_4.png"
-                    img-alt
-                    width="620"
-                    height="400"
-                ></b-carousel-slide>
-                <b-carousel-slide 
-                    img-src="~assets/img/portfolio/DataVault/DV_3.png" 
-                    img-alt 
-                    width="620" 
-                    height="400"
-                ></b-carousel-slide>
-                    <b-carousel-slide
-                    img-src="~assets/img/portfolio/DataVault/DV_2.png"
-                    img-alt
-                    width="620"
-                    height="400"
-                ></b-carousel-slide>
-                <b-carousel-slide
-                    img-src="~assets/img/portfolio/DataVault/DV_1.png"
-                    img-alt
-                    width="620"
-                    height="400"
-                ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/DataVault/DV_6.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/DataVault/DV_5.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/DataVault/DV_4.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/DataVault/DV_3.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/DataVault/DV_2.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/DataVault/DV_1.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
             </b-carousel>
           </b-col>
           <b-col cols="10" class="modal_Descrip">
             <h3>Project Description:</h3>
             <p>
-              This site was a complete development of the design of the website. Full multiple page website with multiple animations.
+              This site was a complete development of the design of the website.
+              Full multiple page website with multiple animations.
             </p>
-            
           </b-col>
-         
+
           <b-col cols="10" class="modal_Details">
             <h3>Project Details:</h3>
             <ul class="list-unstyled project-details">
               <li>
                 <strong>Client:</strong>
-                <a href="https://www.jimsadlermusic.com">DataVault</a>
+                <a href="https://dvt.cloud">DataVault</a>
               </li>
               <li>
-                <strong>Services:</strong> Website Development, Animations, Making site CMS ready
+                <strong>Services:</strong> Website Development, Animations,
+                Making site CMS ready
               </li>
-              <li>
-                <strong>Date:</strong> August 1 2019 - August 18 2019
-              </li>
+              <li><strong>Date:</strong> August 1 2019 - August 18 2019</li>
             </ul>
           </b-col>
         </b-row>
@@ -94,7 +98,7 @@ export default {
     }
   },
   methods: {
-      onSlideStart(slide) {
+    onSlideStart(slide) {
       this.sliding = true
     },
     onSlideEnd(slide) {
@@ -105,11 +109,11 @@ export default {
 </script>
 <style scoped>
 .carousel-item {
-    margin-left: 1%;
+  margin-left: 1%;
 }
-.modal_Descrip, .modal_Details {
-    margin-left: 3%;
-    margin-top: 1%;
+.modal_Descrip,
+.modal_Details {
+  margin-left: 3%;
+  margin-top: 1%;
 }
-
 </style>
--- components/Modals/Modal5a.vue
+++ components/Modals/Modal5a.vue
@@ -1,82 +1,88 @@
 <template>
-    <div class="modal_exp">
-        <b-modal id="modal-scrollable5" scrollable size="xl" title="Angry Apples Marketing Website">
-        <b-container fluid>
-            <b-row>
-            <b-col cols="12">
-                <b-carousel
-                id="modal_carousel-5"
-                :v-model="VmodelType"
-                :interval="3000"
-                background="transparent"
-                img-width="320"
-                img-height="400"
-                class="modalCarousel"
-                fade
-                style="text-shadow: 1px 1px 2px #333;"
-                @sliding-start="onSlideStart"
-                @sliding-end="onSlideEnd"
-                >
-                    <b-carousel-slide 
-                        img-alt 
-                        img-src="~assets/img/portfolio/AApples/AA_5.png" 
-                        
-                    ></b-carousel-slide>
-                    <b-carousel-slide
-                        img-src="~assets/img/portfolio/AApples/AA_4.png"
-                        img-alt
-                        width="620"
-                        height="400"
-                    ></b-carousel-slide>
-                    <b-carousel-slide
-                        img-src="~assets/img/portfolio/AApples/AA_3.png"
-                        img-alt
-                        width="620"
-                        height="400"
-                    ></b-carousel-slide>
-                    <b-carousel-slide
-                        img-src="~assets/img/portfolio/AApples/AA_2.png"
-                        img-alt
-                        width="620"
-                        height="400"
-                    ></b-carousel-slide>
-                    <b-carousel-slide
-                        img-src="~assets/img/portfolio/AApples/AA_1.png"
-                        img-alt
-                        width="620"
-                        height="400"
-                    ></b-carousel-slide>
-                    
-                </b-carousel>
-                </b-col>
-                <b-col cols="10" class="modal_Descrip">
-                <h3>Project Description:</h3>
-                <p>
-                    This project includes a single page template. It has a video header, multiple video and image galleries, a 360 degree viewer, and custom designed sections to present
-                pricing packages.
-                </p>
-                <p>This site also includes a custom drone animation that pulls each section of the site into view.</p>
-                </b-col>
-                
-                <b-col cols="10" class="modal_Details">
-                <h3>Project Details:</h3>
-                <ul class="list-unstyled project-details">
-                    <li>
-                    <strong>Client:</strong>
-                    <a href="https://www.jimsadlermusic.com">Drones iVue</a>
-                    </li>
-                    <li>
-                    <strong>Services:</strong> Web Design, Development, animation design, implementation of 360 player/viewer
-                    </li>
-                    <li>
-                    <strong>Date:</strong> May 2017
-                    </li>
-                </ul>
-                </b-col>
-            </b-row>
-            </b-container>
-        </b-modal>
-    </div>
+  <div class="modal_exp">
+    <b-modal
+      id="modal-scrollable5"
+      scrollable
+      size="xl"
+      title="Angry Apples Marketing Website"
+    >
+      <b-container fluid>
+        <b-row>
+          <b-col cols="12">
+            <b-carousel
+              id="modal_carousel-5"
+              :v-model="VmodelType"
+              :interval="3000"
+              background="transparent"
+              img-width="320"
+              img-height="400"
+              class="modalCarousel"
+              fade
+              style="text-shadow: 1px 1px 2px #333;"
+              @sliding-start="onSlideStart"
+              @sliding-end="onSlideEnd"
+            >
+              <b-carousel-slide
+                img-alt
+                img-src="~assets/img/portfolio/AApples/AA_5.png"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/AApples/AA_4.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/AApples/AA_3.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/AApples/AA_2.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                img-src="~assets/img/portfolio/AApples/AA_1.png"
+                img-alt
+                width="620"
+                height="400"
+              ></b-carousel-slide>
+            </b-carousel>
+          </b-col>
+          <b-col cols="10" class="modal_Descrip">
+            <h3>Project Description:</h3>
+            <p>
+              This project includes a single page template. It has a video
+              header, multiple video and image galleries, a 360 degree viewer,
+              and custom designed sections to present pricing packages.
+            </p>
+            <p>
+              This site also includes a custom drone animation that pulls each
+              section of the site into view.
+            </p>
+          </b-col>
+
+          <b-col cols="10" class="modal_Details">
+            <h3>Project Details:</h3>
+            <ul class="list-unstyled project-details">
+              <li>
+                <strong>Client:</strong>
+                <a>Angry Apples</a>
+              </li>
+              <li>
+                <strong>Services:</strong> Web Design, Development, animation
+                design, implementation of 360 player/viewer
+              </li>
+              <li><strong>Date:</strong> May 2017</li>
+            </ul>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-modal>
+  </div>
 </template>
 <script>
 export default {
@@ -89,7 +95,7 @@ export default {
     }
   },
   methods: {
-      onSlideStart(slide) {
+    onSlideStart(slide) {
       this.sliding = true
     },
     onSlideEnd(slide) {
@@ -100,11 +106,11 @@ export default {
 </script>
 <style scoped>
 .carousel-item {
-    margin-left: 1%;
+  margin-left: 1%;
 }
-.modal_Descrip, .modal_Details {
-    margin-left: 3%;
-    margin-top: 1%;
+.modal_Descrip,
+.modal_Details {
+  margin-left: 3%;
+  margin-top: 1%;
 }
-
 </style>
