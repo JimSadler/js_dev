052ed5f2c7f54f0badc16e51bc63e06a1bc39c4e
Jim Sadler <jim@jsdevelopment.tech>
2022-09-03 19:40:34 -0400

    hid the project page. it is still wip

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,9 +30,9 @@
         <b-navbar-nav class="ml-auto">
           <b-nav-item router-tag="a" to="/services">Services</b-nav-item>
           <b-nav-item to="/work" router-tag="a">Work</b-nav-item>
-          <b-nav-item
+          <!-- <b-nav-item
             ><nuxt-link to="/projects">Projects</nuxt-link></b-nav-item
-          >
+          > -->
           <b-nav-item router-tag="a" to="/testimonials"
             >Testimonials</b-nav-item
           >
